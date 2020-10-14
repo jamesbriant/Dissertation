0fe23ef3db7d01a5ea15a311f3f0fe22e8694451
diggerb <james@briant.co.uk>
2020-10-14 17:57:45 +0100

    split up useful functions

---
+++ src/functions/rejection-sampling-functions.R
@@ -0,0 +1,30 @@
+drawFromg <- function(n){
+  # g ~ Uniform[0,1]
+  return(runif(n, 0 , 1))  
+}
+
+f <- function(x){
+  # Beta distribution with alpha=2 & beta=5
+  # Recall Beta distribution is only define on [0,1]
+  
+  alpha <- 2
+  beta <- 5
+  
+  numerator <- x^(alpha-1) * (1-x)^(beta-1) * gamma(alpha + beta)
+  denominator <- gamma(alpha) * gamma(beta)
+  
+  return(numerator/denominator)
+}
+
+drawSample <- function(n){
+  y <- runif(n, 0, 2.5)
+  # f(x) < 2.5 for x on [0,1]. Hence this is NOT most computationally efficient!
+  
+  # This is the g function!
+  x <- drawFromg(n)
+  
+  fx <- f(x)
+  
+  return(x[y < fx])
+}
+
--- src/rejection-sampling.R
+++ src/rejection-sampling.R
@@ -1,50 +1,13 @@
 #########################################################################
 # Sample f=Beta(2,5) from g=Unif(0,1) using Rejection Sampling
 
-drawFromg <- function(n){
-  # g ~ Uniform[0,2.5]
-  return(runif(n, 0 , 2.5))  
-  # f(x) < 2.5 for x on [0,1]. Hence this is NOT most computationally efficient!
-}
+source('functions/rejection-sampling-functions.R')
 
-f <- function(x){
-  # Beta distribution with alpha=2 & beta=5
-  # Recall Beta distribution is only define on [0,1]
-  
-  alpha <- 2
-  beta <- 5
-  
-  numerator <- x^(alpha-1) * (1-x)^(beta-1) * gamma(alpha + beta)
-  denominator <- gamma(alpha) * gamma(beta)
-  
-  return(numerator/denominator)
-}
-
-drawSample <- function(n){
-  x <- runif(n, 0, 1)
-  
-  # This is the g function!
-  y <- drawFromg(n)
-  
-  fx <- f(x)
-  
-  return(x[y < fx])
-}
-
-
-data <- drawSample(10000)
-hist(data, freq=FALSE, xlab="x", main="Beta(2,5) Distribution from 10,000 Draws")
+data <- drawSample(2500)
+hist(data, freq=FALSE, xlab="x", main="Beta(2,5) Distribution from 2500 Draws")
 x <- seq(from=0, to=1, length.out=250)
 lines(x, dbeta(x, 2, 5), col="red")
 legend(0.5, 2.4, legend=c("Rejection Sampling", "True distribution"), pch="-", col=c("black", "red"))
-
-
-
-
-
-
-
-
-
+rug(data)
 
 
