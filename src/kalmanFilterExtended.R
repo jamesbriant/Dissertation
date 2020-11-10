b031f484b8e9f3ebb4c2c21171e74ab29d1a7268
diggerb <james@briant.co.uk>
2020-11-10 21:20:46 +0000

    Create kalmanFilterExtended.R

---
+++ src/kalmanFilterExtended.R
@@ -0,0 +1,260 @@
+library(mvtnorm)  # for multivariate normal - rmvnorm()
+library(pracma)   # for jacobian()
+
+GenerateKalmanData <- function(f, g, X0, W, V, n=20){
+  # Generate the unobserved system X(t) and the observed data Y(t)
+  # X(t+1) = fX(t) + W
+  # Y(t) = gX(t) + V
+  #
+  # INPUTS:
+  #   f:    matrix
+  #   g:    matrix
+  #   x0:   start value
+  #   W:    variance matrix
+  #   V:    variance matrix
+  #   n:    number of data points to be produced
+  # OUTPUTS:
+  #   X:    list of system states
+  #   Y:    list of observed states
+  
+  f.matrix <- as.matrix(f)
+  g.matrix <- as.matrix(g)
+  X0.matrix <- as.matrix(X0)
+  W.matrix <- as.matrix(W)
+  V.matrix <- as.matrix(V)
+  
+  # Generate system data X and observations Y
+  X <- list()
+  X[[1]] <- t(rmvnorm(1, f.matrix %*% X0.matrix, W.matrix))
+  Y <- list()
+  Y[[1]] <- t(rmvnorm(1, g.matrix %*% X[[1]], V.matrix))
+  for (i in 2:n){
+    X[[i]] <- t(rmvnorm(1, f.matrix %*% X[[i-1]], W.matrix))
+    Y[[i]] <- t(rmvnorm(1, g.matrix %*% X[[i]], V.matrix))
+  }
+  
+  return(list(X=X, Y=Y))
+}
+
+GenerateExtendedKalmanData <- function(f, g, X0, W, V, n=20){
+  # Generate the unobserved system X(t) and the observed data Y(t)
+  # X(t+1) = fX(t) + W
+  # Y(t) = gX(t) + V
+  #
+  # INPUTS:
+  #   f:    function
+  #   g:    function
+  #   x0:   start value
+  #   W:    variance matrix
+  #   V:    variance matrix
+  #   n:    number of data points to be produced
+  # OUTPUTS:
+  #   X:    list of system states
+  #   Y:    list of observed states
+  
+  X0.matrix <- as.matrix(X0)
+  W.matrix <- as.matrix(W)
+  V.matrix <- as.matrix(V)
+  
+  # Generate system data X and observations Y
+  X <- list()
+  X[[1]] <- t(rmvnorm(1, f(c(X0.matrix, 1)), W.matrix))
+  Y <- list()
+  Y[[1]] <- t(rmvnorm(1, g(c(X[[1]], 1)), V.matrix))
+  for (i in 2:n){
+    X[[i]] <- t(rmvnorm(1, f(c(X[[i-1]], i)), W.matrix))
+    Y[[i]] <- t(rmvnorm(1, g(c(X[[i]], i)), V.matrix))
+  }
+  
+  return(list(X=X, Y=Y))
+}
+
+CalcKalmanGain <- function(g, R, Q){
+  if(dim(Q)[1]==1){
+    return(1/as.numeric(Q) * g %*% R)
+  }
+  else{
+    return(g %*% R %*% solve(Q))
+  }
+}
+
+ApplyKalmanFilter <- function(Y, f, g, m0, C0, W, V){
+  # INPUTS:
+  #   Y:    list of observed states
+  #   f:    matrix
+  #   g:    matrix
+  #   m0:   initial estimate for mean of distribution of X
+  #   C0:   initial estimate for variance of distribution of X
+  #   W:    variance matrix
+  #   V:    variance matrix
+  # OUTPUTS:
+  #   a:    list of priors for the distribution mean of X given y at t-1
+  #   m:    list of posteriors for the distribution mean of X given y at t
+  #   C:    list of posterior variance matrices for distn  of X at time t
+  #   R:    list of prior variance matrices for distn of X at time t+1
+  #   Q:    list of prior variance matrices for distn of Y at time t+1
+  #   K:    list of Kalman gain values for each time t
+  
+  f.matrix <- as.matrix(f)
+  g.matrix <- as.matrix(g)
+  m0.matrix <- as.matrix(m0)
+  C0.matrix <- as.matrix(C0)
+  W.matrix <- as.matrix(W)
+  V.matrix <- as.matrix(V)
+  
+  n <- length(Y)
+  p <- nrow(f.matrix)
+  
+  a <- list()
+  m <- list()
+  C <- list()
+  R <- list()
+  Q <- list()
+  K <- list()
+  
+  # Perform initial calculation using initial conditions
+  a[[1]] <- f.matrix %*% m0.matrix
+  R[[1]] <- f.matrix %*% C0.matrix %*% t(f.matrix) + W.matrix
+  Q[[1]] <- g.matrix %*% R[[1]] %*% t(g.matrix) + V.matrix
+  
+  K[[1]] <- CalcKalmanGain(g.matrix, R[[1]], Q[[1]])
+  m[[1]] <- a[[1]] + t(K[[1]]) %*% (Y[[1]] - g.matrix %*% a[[1]])
+  C[[1]] <- (diag(p) -  t(K[[1]]) %*% g.matrix) %*% R[[1]]
+  
+  for(t in 2:n){
+    # predict
+    a[[t]] <- f.matrix %*% m[[t-1]]
+    R[[t]] <- f.matrix %*% C[[t-1]] %*% t(f.matrix) + W.matrix
+    Q[[t]] <- g.matrix %*% R[[t]] %*% t(g.matrix) + V.matrix
+    
+    # update
+    K[[t]] <- CalcKalmanGain(g.matrix, R[[t]], Q[[t]])
+    m[[t]] <- a[[t]] + t(K[[t]]) %*% (Y[[t]] - g.matrix %*% a[[t]])
+    C[[t]] <- (diag(p) - t(K[[t]]) %*% g.matrix) %*% R[[t]]
+  }
+  
+  return(list(a=a, m=m, C=C, R=R, Q=Q, K=K))
+}
+
+ApplyExtendedKalmanFilter <- function(Y, f, g, m0, C0, W, V){
+  # INPUTS:
+  #   Y:    list of observed states
+  #   f:    function
+  #   g:    function
+  #   m0:   initial estimate for mean of distribution of X
+  #   C0:   initial estimate for variance of distribution of X
+  #   W:    variance matrix
+  #   V:    variance matrix
+  # OUTPUTS:
+  #   a:    list of priors for the distribution mean of X given y at t-1
+  #   m:    list of posteriors for the distribution mean of X given y at t
+  #   C:    list of posterior variance matrices for distn  of X at time t
+  #   R:    list of prior variance matrices for distn of X at time t+1
+  #   Q:    list of prior variance matrices for distn of Y at time t+1
+  #   K:    list of Kalman gain values for each time t
+  
+  m0.matrix <- as.matrix(m0)
+  C0.matrix <- as.matrix(C0)
+  W.matrix <- as.matrix(W)
+  V.matrix <- as.matrix(V)
+  
+  f.m0 <- as.matrix(f(c(m0, 1)))
+  g.fm0 <- as.matrix(g(c(f.m0, 1)))
+  df.m0 <- jacobian(f, c(m0, 1))
+  dg.fm0 <- jacobian(g, c(f.m0, 1))
+  
+  n <- length(Y)
+  p <- dim(dg.fm0)[2]
+  
+  a <- list()
+  m <- list()
+  C <- list()
+  R <- list()
+  Q <- list()
+  K <- list()
+  
+  # Perform initial calculation using initial conditions
+  a[[1]] <- f.m0
+  R[[1]] <- df.m0 %*% C0.matrix %*% t(df.m0) + W.matrix
+  Q[[1]] <- dg.fm0 %*% R[[1]] %*% t(dg.fm0) + V.matrix
+  
+  K[[1]] <- CalcKalmanGain(dg.fm0, R[[1]], Q[[1]])
+  m[[1]] <- a[[1]] + t(K[[1]]) %*% (Y[[1]] - g.fm0)
+  C[[1]] <- (diag(p) -  t(K[[1]]) %*% dg.fm0) %*% R[[1]]
+  
+  for(t in 2:n){
+    f.m <- as.matrix(f(c(m[[t-1]], t)))
+    g.fm <- as.matrix(g(c(f.m, t)))
+    df.m <- jacobian(f, c(m[[t-1]], t))
+    dg.fm <- jacobian(g, c(f.m, t))
+    
+    # predict
+    a[[t]] <- f.m
+    R[[t]] <- df.m %*% C[[t-1]] %*% t(df.m) + W.matrix
+    Q[[t]] <- dg.fm %*% R[[t]] %*% t(dg.fm) + V.matrix
+    
+    # update
+    K[[t]] <- CalcKalmanGain(dg.fm, R[[t]], Q[[t]])
+    m[[t]] <- a[[t]] + t(K[[t]]) %*% (Y[[t]] - g.fm)
+    C[[t]] <- (diag(p) - t(K[[t]]) %*% dg.fm) %*% R[[t]]
+  }
+  
+  return(list(a=a, m=m, C=C, R=R, Q=Q, K=K))
+}
+
+
+
+
+#########################################################################
+# Example: X-1D; Y-1D;
+
+f <- function(x){
+  X <- x[1]
+  t <- x[2]
+  return(3*cos(pi*t/10))
+}
+
+g <- function(x){
+  X <- x[1]
+  t <- x[2]
+  return(X)
+}
+
+X0 <- 3
+
+# Set variances
+W <- 0.01
+V <- 0.03
+
+n <- 30
+data <- GenerateExtendedKalmanData(f, g, X0, W, V, n)
+X <- data$X
+Y <- data$Y
+
+
+
+# Initial estimates
+m0 <- X0 # mean
+C0 <- W # variance
+
+kalmanSolution <- ApplyExtendedKalmanFilter(Y, f, g, m0, C0, W, V)
+
+
+
+plot(1:n, unlist(X), type="l", xlab="time", ylab="state")
+lines(1:n, unlist(kalmanSolution$m), col="red")
+points(1:n, unlist(kalmanSolution$m), col="red")
+points(1:n, unlist(kalmanSolution$a), col="blue")
+legend("topleft", 
+       legend=c("X(t) - Unobserved System",
+                "m(t) - Posterior",
+                "a(t) - Prior"), 
+       col=c("black", "red", "blue"), 
+       lty=c(1, 1, 0), 
+       pch=c(26, 1, 1)
+)
+
+
+
+
+
